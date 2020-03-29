4598234e35d022d5a1586867edcf97e073fd446e
Alfred <alfred@kyles-mbp.gateway.sonic.net>
2020-03-28 17:58:17 -0700

    deployment adjustment

--- server.js
+++ server.js
@@ -11,19 +11,26 @@ app.use(bodyParser.json());
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/streamers/random', (req, res) => {
-    axios.get('http://localhost:8001/streamers/random')
+    axios.get('http://54.176.192.211/streamers/random')
         .then((result) => {
             res.status(200).json(result.data);
         })
         .catch(err =>  res.status(400).send(err))
 });
 
+app.get('/api/get', (req, res) => {
+    axios.get('http://18.144.87.148/api/get')
+        .then((result) => {
+            res.status(200).json(result.data);
+        })
+        .catch(err =>  res.status(400).send(err))
+});
 
 app.put('/streamers/:name', (req, res) => {
     const streamerName = req.params.name;
     const updateObject = req.body;
     
-    axios.put(`http://localhost:8001/streamers/${streamerName}`, updateObject)
+    axios.put(`http://54.176.192.211/streamers/${streamerName}`, updateObject)
       .then((result) => {
         res.send(result.data);
       })
